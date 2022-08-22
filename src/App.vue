7e087bcb6d029a15e7242ed0312c5f73c23b37bb
Yannick Holzenkamp <yannick@holzenkamp.me>
2022-08-22 11:17:00 +0200

    add input/output

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
   import { BalApp, BalFooter, BalHeading, BalButton } from '@baloise/design-system-components-vue'
+  import Hans from './components/Hans.vue'
+
+  function aaaa(str: string) {
+    alert(str)
+  }
 </script>
 
 <template>
   <BalApp class="has-sticky-footer has-background">
     <header>
-      <!-- Header content -->
+      <bal-navbar>
+          <bal-navbar-brand simple>
+              <bal-logo brand="group" color="white"></bal-logo>
+              <bal-text><strong>Lunchdate</strong> Vue.js</bal-text>
+          </bal-navbar-brand>
+      </bal-navbar>
     </header>
     <main class="container">
-      <BalHeading>Hello World!</BalHeading>
-      <BalButton>Button</BalButton>
-      <!-- Page content -->
+      <bal-tabs interface="tabs" value="tab-a">
+        <bal-tab-item value="tab-a" label="Hans">
+          <Hans foo="gügüsli" @clckd="aaaa"/>
+        </bal-tab-item>
+        <bal-tab-item value="tab-b" label="Gugus">
+          <BalHeading>Gugus</BalHeading>
+        </bal-tab-item>
+      </bal-tabs>
     </main>
     <BalFooter>
       <div class="container">
---
+++ src/components/Hans.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { BalApp, BalFooter, BalHeading, BalButton, BalInput } from '@baloise/design-system-components-vue'
+  const a = 'hansili';
+
+  function gugus() {
+    alert('wer das liest ist doof')
+  }
+
+  const props = defineProps({
+    foo: String
+  })
+
+  const emit = defineEmits(['clckd'])
+</script>
+
+<template>
+  <div class="hans">
+    <BalHeading>{{foo}}</BalHeading>
+    <BalInput v-model="myInput" />
+    <bal-button @click="this.$emit('clckd', myInput)">Press me</bal-button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+</style>
